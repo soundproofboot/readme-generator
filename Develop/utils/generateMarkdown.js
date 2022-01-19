344e571647032b7ecd9f2552074f23eecc767006
C Bares <colinedwinbares@gmail.com>
2022-01-19 17:07:23 -0600

    Set up outline of md file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,9 +12,34 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+    # ${data.title}
 
-`;
+    ## Description
+
+    ${data.description}
+
+    ## Table of Contents
+
+    [Installation](installation)
+    [Usage](usage)
+    [License](license)
+    [Contributing](contributing)
+    [Tests](tests)
+    [Questions](questions)
+
+    ## Installation
+
+    ## Usage
+
+    ## License
+
+    ## Contributing
+
+    ## Tests
+
+    ## Questions
+    `;
 }
 
 module.exports = generateMarkdown;
